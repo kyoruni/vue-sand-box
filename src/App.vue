5fe7c75d7e2c161aeb29d2f3e9d1d02b8383d58f
kyoruni <kyoruni@gmail.com>
2020-01-09 10:09:29 +0900

    :wrench: bootstrap導入

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <button class="btn btn-info">test</button>
     <router-view/>
   </div>
 </template>
