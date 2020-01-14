fbac1569bfa559cd0e10360d64e4936e540005fa
kyoruni <kyoruni@gmail.com>
2020-01-14 11:44:48 +0900

    #2 :wrench: カテゴリーではなく、IDを表示する
    その方が扱いやすそう

--- src/views/Draggable.vue
+++ src/views/Draggable.vue
@@ -7,7 +7,9 @@
           <draggable tag :options="{group:'foods'}">
             <b-list-group-item v-for="fruit in fruits" :key="fruit.no">
               {{ fruit.name }}
-              <b-badge pill variant="info" class="pull-right">{{ fruit.categoryName }}</b-badge>
+              <b-badge pill variant="info" class="pull-right">
+                ID: {{ fruit.no }}
+              </b-badge>
             </b-list-group-item>
           </draggable>
         </b-list-group>
@@ -18,7 +20,9 @@
           <draggable tag :options="{group:'foods'}">
             <b-list-group-item v-for="vegetable in vegetables" :key="vegetable.no">
               {{ vegetable.name }}
-              <b-badge pill variant="info">{{ vegetable.categoryName }}</b-badge>
+              <b-badge pill variant="info">
+                ID: {{ vegetable.no }}
+              </b-badge>
             </b-list-group-item>
           </draggable>
         </b-list-group>
@@ -29,7 +33,9 @@
           <draggable tag>
             <b-list-group-item v-for="weekday in weekdays" :key="weekday.no">
               {{ weekday.name }}
-              <b-badge pill variant="info">{{ weekday.categoryName }}</b-badge>
+              <b-badge pill variant="info">
+                ID: {{ weekday.no }}
+              </b-badge>
             </b-list-group-item>
           </draggable>
         </b-list-group>
@@ -40,7 +46,9 @@
           <draggable tag>
             <b-list-group-item v-for="tea in teas" :key="tea.no">
               {{ tea.name }}
-              <b-badge pill variant="info">{{ tea.categoryName }}</b-badge>
+              <b-badge pill variant="info">
+                ID: {{ tea.no }}
+              </b-badge>
             </b-list-group-item>
           </draggable>
         </b-list-group>
@@ -59,30 +67,30 @@ export default {
   data () {
     return {
       fruits: [
-        { no: 1, name: 'りんご', categoryName: '果物' },
-        { no: 2, name: 'いちご', categoryName: '果物' },
-        { no: 3, name: 'ぶどう', categoryName: '果物' },
-        { no: 4, name: 'みかん', categoryName: '果物' }
+        { no: 1, name: 'りんご' },
+        { no: 2, name: 'いちご' },
+        { no: 3, name: 'ぶどう' },
+        { no: 4, name: 'みかん' }
       ],
       vegetables: [
-        { no: 5, name: 'キャベツ', categoryName: '野菜' },
-        { no: 6, name: 'にんじん', categoryName: '野菜' },
-        { no: 7, name: 'トマト', categoryName: '野菜' },
-        { no: 8, name: 'レタス', categoryName: '野菜' }
+        { no: 5, name: 'キャベツ' },
+        { no: 6, name: 'にんじん' },
+        { no: 7, name: 'トマト' },
+        { no: 8, name: 'レタス' }
       ],
       weekdays: [
-        { no: 9, name: '月曜日', categoryName: '曜日' },
-        { no: 10, name: '火曜日', categoryName: '曜日' },
-        { no: 11, name: '水曜日', categoryName: '曜日' },
-        { no: 12, name: '木曜日', categoryName: '曜日' },
-        { no: 13, name: '金曜日', categoryName: '曜日' },
-        { no: 14, name: '土曜日', categoryName: '曜日' },
-        { no: 15, name: '日曜日', categoryName: '曜日' }
+        { no: 9, name: '月曜日' },
+        { no: 10, name: '火曜日' },
+        { no: 11, name: '水曜日' },
+        { no: 12, name: '木曜日' },
+        { no: 13, name: '金曜日' },
+        { no: 14, name: '土曜日' },
+        { no: 15, name: '日曜日' }
       ],
       teas: [
-        { no: 16, name: '午後の紅茶', categoryName: '紅茶' },
-        { no: 17, name: 'リプトン', categoryName: '紅茶' },
-        { no: 18, name: '紅茶花伝', categoryName: '紅茶' }
+        { no: 16, name: '午後の紅茶' },
+        { no: 17, name: 'リプトン' },
+        { no: 18, name: '紅茶花伝' }
       ]
     }
   }
