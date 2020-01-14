1d02079d649efbceaf291f6e1dcd6ac6abacbad1
kyoruni <kyoruni@gmail.com>
2020-01-14 10:35:17 +0900

    #2 :wrench: リスト作成

--- src/views/Draggable.vue
+++ src/views/Draggable.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="draggable">
-    <b-row>
-      <b-col cols="3" class="bg-info">
-        1column
+    <b-row class="mt-2">
+      <b-col cols="3">
+        <b-list-group>
+          <b-list-group-item>りんご</b-list-group-item>
+          <b-list-group-item>いちご</b-list-group-item>
+          <b-list-group-item>ぶどう</b-list-group-item>
+          <b-list-group-item>みかん</b-list-group-item>
+        </b-list-group>
       </b-col>
-      <b-col cols="3" class="bg-warning">
-        2column
+      <b-col cols="3">
+        <b-list-group>
+          <b-list-group-item>キャベツ</b-list-group-item>
+          <b-list-group-item>にんじん</b-list-group-item>
+          <b-list-group-item>トマト</b-list-group-item>
+          <b-list-group-item>レタス</b-list-group-item>
+        </b-list-group>
       </b-col>
-      <b-col cols="3" class="bg-info">
-        3column
+      <b-col cols="3">
+        <b-list-group>
+          <b-list-group-item>月曜日</b-list-group-item>
+          <b-list-group-item>火曜日</b-list-group-item>
+          <b-list-group-item>水曜日</b-list-group-item>
+          <b-list-group-item>木曜日</b-list-group-item>
+        </b-list-group>
       </b-col>
-      <b-col cols="3" class="bg-warning">
-        4column
+      <b-col cols="3">
+        <b-list-group>
+          <b-list-group-item>午後の紅茶</b-list-group-item>
+          <b-list-group-item>リプトン</b-list-group-item>
+          <b-list-group-item>紅茶花伝</b-list-group-item>
+        </b-list-group>
       </b-col>
     </b-row>
   </div>
