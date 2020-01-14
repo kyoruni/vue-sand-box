ade69ac26a82393edd54e069f851839fdab38bc3
kyoruni <kyoruni@gmail.com>
2020-01-14 10:30:06 +0900

    #2 :wrench: カラム分け

--- src/views/Draggable.vue
+++ src/views/Draggable.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="draggable">
-    ドラッグ＆ドロップお試し用
+    <b-row>
+      <b-col cols="3" class="bg-info">
+        1column
+      </b-col>
+      <b-col cols="3" class="bg-warning">
+        2column
+      </b-col>
+      <b-col cols="3" class="bg-info">
+        3column
+      </b-col>
+      <b-col cols="3" class="bg-warning">
+        4column
+      </b-col>
+    </b-row>
   </div>
 </template>
