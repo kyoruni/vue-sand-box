b9521f9b8476deeeb4b609544c374e43483d676c
kyoruni <kyoruni@gmail.com>
2020-01-14 10:57:01 +0900

    #2 :wrench: リストは配列から表示

--- src/views/Draggable.vue
+++ src/views/Draggable.vue
@@ -1,37 +1,65 @@
 <template>
   <div class="draggable">
     <b-row class="mt-2">
+      <!-- フルーツ -->
       <b-col cols="3">
         <b-list-group>
-          <b-list-group-item>りんご</b-list-group-item>
-          <b-list-group-item>いちご</b-list-group-item>
-          <b-list-group-item>ぶどう</b-list-group-item>
-          <b-list-group-item>みかん</b-list-group-item>
+          <b-list-group-item v-for="fruit in fruits" :key="fruit.no">{{ fruit.name }}</b-list-group-item>
         </b-list-group>
       </b-col>
+      <!-- 野菜 -->
       <b-col cols="3">
         <b-list-group>
-          <b-list-group-item>キャベツ</b-list-group-item>
-          <b-list-group-item>にんじん</b-list-group-item>
-          <b-list-group-item>トマト</b-list-group-item>
-          <b-list-group-item>レタス</b-list-group-item>
+          <b-list-group-item v-for="vegetable in vegetables" :key="vegetable.no">{{ vegetable.name }}</b-list-group-item>
         </b-list-group>
       </b-col>
+      <!-- 曜日 -->
       <b-col cols="3">
         <b-list-group>
-          <b-list-group-item>月曜日</b-list-group-item>
-          <b-list-group-item>火曜日</b-list-group-item>
-          <b-list-group-item>水曜日</b-list-group-item>
-          <b-list-group-item>木曜日</b-list-group-item>
+          <b-list-group-item v-for="weekday in weekdays" :key="weekday.no">{{ weekday.name }}</b-list-group-item>
         </b-list-group>
       </b-col>
+      <!-- 紅茶 -->
       <b-col cols="3">
         <b-list-group>
-          <b-list-group-item>午後の紅茶</b-list-group-item>
-          <b-list-group-item>リプトン</b-list-group-item>
-          <b-list-group-item>紅茶花伝</b-list-group-item>
+          <b-list-group-item v-for="tea in teas" :key="tea.no">{{ tea.name }}</b-list-group-item>
         </b-list-group>
       </b-col>
     </b-row>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      fruits: [
+        { no: 1, name: 'りんご' },
+        { no: 2, name: 'いちご' },
+        { no: 3, name: 'ぶどう' },
+        { no: 4, name: 'みかん' }
+      ],
+      vegetables: [
+        { no: 5, name: 'キャベツ' },
+        { no: 6, name: 'にんじん' },
+        { no: 7, name: 'トマト' },
+        { no: 8, name: 'レタス' }
+      ],
+      weekdays: [
+        { no: 9, name: '月曜日' },
+        { no: 10, name: '火曜日' },
+        { no: 11, name: '水曜日' },
+        { no: 12, name: '木曜日' },
+        { no: 13, name: '金曜日' },
+        { no: 14, name: '土曜日' },
+        { no: 15, name: '日曜日' }
+      ],
+      teas: [
+        { no: 16, name: '午後の紅茶' },
+        { no: 17, name: 'リプトン' },
+        { no: 18, name: '紅茶花伝' }
+      ]
+    }
+  }
+}
+</script>
