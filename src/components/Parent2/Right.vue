d04453f71ab9a55f97bc12f2873f736981f985c7
kyoruni <kyoruni@gmail.com>
2020-01-17 15:49:03 +0900

    :wrench: #8 3タイプのボタン追加

--- src/components/Parent2/Right.vue
+++ src/components/Parent2/Right.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="right bg-info">
-    Parent2 Right
+  <div class="right">
+    <h6>Parent2 Right</h6>
+    <b-button-group class="d-flex">
+      <b-button variant="outline-danger" class="w-100" @click="clickFireButton">
+        ほのお
+      </b-button>
+      <b-button variant="outline-info" class="w-100" @click="clickWaterButton">
+        みず
+      </b-button>
+      <b-button variant="outline-success" class="w-100" @click="clickGrassButton">
+        くさ
+      </b-button>
+    </b-button-group>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    clickFireButton () {
+      alert('fire')
+    },
+    clickWaterButton () {
+      alert('water')
+    },
+    clickGrassButton () {
+      alert('grass')
+    }
+  }
+}
+</script>
