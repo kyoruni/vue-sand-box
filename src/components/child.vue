f958844f8ba8afe727578e96e5ac5893af764cf5
kyoruni <kyoruni@gmail.com>
2020-01-17 11:57:20 +0900

    :wrench: #8 子コンポーネント作成

---
+++ src/components/child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child">
+    <h5 class="mt-2">Child</h5>
+    <b-button variant="info" @click="childButton">子ボタン</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    childButton () {
+      alert('子コンポーネント')
+    }
+  }
+}
+</script>
--- src/views/Parent.vue
+++ src/views/Parent.vue
@@ -2,11 +2,19 @@
   <div class="parent">
     <h5 class="mt-2">Parent</h5>
     <b-button variant="info" @click="parentButton">親ボタン</b-button>
+
+    <!-- 子コンポーネント -->
+    <child></child>
   </div>
 </template>
 
 <script>
+import child from '../components/child.vue'
+
 export default {
+  components: {
+    child
+  },
   methods: {
     parentButton () {
       alert('親コンポーネント')
