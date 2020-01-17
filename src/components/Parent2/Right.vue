b2bf2ac2464b391d8c20c9d65a15ab2564056a9b
kyoruni <kyoruni@gmail.com>
2020-01-17 16:06:41 +0900

    :wrench: #8 押したボタンにactiveクラスを付与

--- src/components/Parent2/Right.vue
+++ src/components/Parent2/Right.vue
@@ -2,13 +2,13 @@
   <div class="right">
     <h6>Parent2 Right</h6>
     <b-button-group class="d-flex">
-      <b-button variant="outline-danger" class="w-100" @click="clickFireButton">
+      <b-button variant="outline-danger" class="w-100" :class="{'active':showFireType}" @click="clickFireButton">
         ほのお
       </b-button>
-      <b-button variant="outline-info" class="w-100" @click="clickWaterButton">
+      <b-button variant="outline-info" class="w-100" :class="{'active':showWaterType}" @click="clickWaterButton">
         みず
       </b-button>
-      <b-button variant="outline-success" class="w-100" @click="clickGrassButton">
+      <b-button variant="outline-success" class="w-100" :class="{'active':showGrassType}" @click="clickGrassButton">
         くさ
       </b-button>
     </b-button-group>
