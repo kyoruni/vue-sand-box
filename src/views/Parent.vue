0c6f624023d271d3413085d105aef847352dca89
kyoruni <kyoruni@gmail.com>
2020-01-17 12:03:58 +0900

    :wrench: #8 親コンポーネントから子コンポーネントのメソッド

--- src/views/Parent.vue
+++ src/views/Parent.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="parent">
     <h5 class="mt-2">Parent</h5>
-    <b-button variant="info" @click="parentButton">親ボタン</b-button>
+    <b-button variant="info" @click="parentButton" class="mr-2">親ボタン</b-button>
+    <b-button variant="outline-info" @click="parentToChildButton">親コンポーネントから子メソッド</b-button>
 
     <!-- 子コンポーネント -->
-    <child></child>
+    <child ref="childComponent"></child>
   </div>
 </template>
 
@@ -18,6 +19,9 @@ export default {
   methods: {
     parentButton () {
       alert('親コンポーネント')
+    },
+    parentToChildButton () {
+      this.$refs.childComponent.childButton()
     }
   }
 }
