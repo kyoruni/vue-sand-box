0cb2944ca86626a697603b0b5707b1471d9fc31c
kyoruni <kyoruni@gmail.com>
2020-01-20 10:29:05 +0900

    :wrench: #2 画面左側 ドラッグで入れ替えできるように

--- src/components/Parent2/Left.vue
+++ src/components/Parent2/Left.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="left bg-warning">
-    Parent2 Left
+  <div class="left">
+    <draggable>
+    <div class="bg-info">header</div>
+    <div class="bg-warning">body</div>
+    <div class="bg-success">footer</div>
+    </draggable>
   </div>
 </template>
+
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+  components: {
+    draggable
+  }
+}
+</script>
