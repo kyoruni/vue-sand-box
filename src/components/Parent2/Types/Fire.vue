c6adb3a1cf0bdc5cc32f15c6112c3755c80d75fb
kyoruni <kyoruni@gmail.com>
2020-01-20 10:44:09 +0900

    :wrench: #2 画面右側 ドラッグで入れ替えできるように

--- src/components/Parent2/Types/Fire.vue
+++ src/components/Parent2/Types/Fire.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="fire">
     <h6 class="mt-2">ほのお</h6>
+
+    <draggable>
+      <!-- item -->
+      <div class="card mb-2">
+        <div class="card-body">とかげポケモン</div>
+        <div class="card-footer">ヒトカゲ</div>
+      </div>
+      <!-- item -->
+      <div class="card mb-2">
+        <div class="card-body">ひねずみポケモン</div>
+        <div class="card-footer">ヒノアラシ</div>
+      </div>
+      <!-- item -->
+      <div class="card mb-2">
+        <div class="card-body">ひよこポケモン</div>
+        <div class="card-footer">アチャモ</div>
+      </div>
+    </draggable>
+
+    <!-- 閉じるボタン -->
     <b-button @click="closeTab" size="sm" class="float-right">
       閉じる
     </b-button>
@@ -8,7 +28,12 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
+  components: {
+    draggable
+  },
   methods: {
     closeTab () {
       this.$emit('close')
