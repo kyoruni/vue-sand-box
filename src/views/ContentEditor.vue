ac7d59d8985ba7b27a1964083e5de9ab8d43ef4a
kyoruni <kyoruni@gmail.com>
2020-01-21 10:18:01 +0900

    :wrench: #16 レイアウト表示

--- src/views/ContentEditor.vue
+++ src/views/ContentEditor.vue
@@ -1,28 +1,47 @@
 <template>
   <div class="content-editor">
     <div class="d-flex">
-      <div class="col-8 bg-info">
-        left
+      <!-- Left -->
+      <div class="col-8">
+        <draggable>
+          <div v-for="layout in layouts" :key="layout.id">
+            <div :class="layout.class">
+              {{ layout.body }}
+            </div>
+          </div>
+        </draggable>
       </div>
       <!-- Right -->
-      <div class="col-4 bg-warning">
-        <b-card v-for="part in parts" :key="part.id" class="mb-2 mx-auto">
-          <b-card-text>
-            {{ part.name }}
-          </b-card-text>
-        </b-card>
+      <div class="col-4">
+        <draggable>
+          <b-card v-for="part in parts" :key="part.id" class="mb-2 mx-auto">
+            <b-card-text>
+              {{ part.name }}
+            </b-card-text>
+          </b-card>
+        </draggable>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
+  components: {
+    draggable
+  },
   data () {
     return {
       parts: [
         { id: 1, name: 'Text' },
         { id: 2, name: 'Button' }
+      ],
+      layouts: [
+        { id: 1, body: 'Header', class: 'bg-info' },
+        { id: 2, body: 'Body', class: 'bg-warning' },
+        { id: 3, body: 'Footer', class: 'bg-success' }
       ]
     }
   }
@@ -31,6 +50,7 @@ export default {
 
 <style scoped>
 .card {
-  max-width: 60%
+  max-width: 60%;
+  cursor: pointer
 }
 </style>
