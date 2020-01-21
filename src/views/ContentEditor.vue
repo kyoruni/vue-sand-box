a5328f2411ef134defaf50384763135d8e0b9aa1
kyoruni <kyoruni@gmail.com>
2020-01-21 10:11:33 +0900

    :wrench: #16 パーツ表示

--- src/views/ContentEditor.vue
+++ src/views/ContentEditor.vue
@@ -4,9 +4,33 @@
       <div class="col-8 bg-info">
         left
       </div>
+      <!-- Right -->
       <div class="col-4 bg-warning">
-        right
+        <b-card v-for="part in parts" :key="part.id" class="mb-2 mx-auto">
+          <b-card-text>
+            {{ part.name }}
+          </b-card-text>
+        </b-card>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      parts: [
+        { id: 1, name: 'Text' },
+        { id: 2, name: 'Button' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  max-width: 60%
+}
+</style>
