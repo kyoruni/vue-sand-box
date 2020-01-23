d6253b48857489dabb3e828c3e6659ebc49442c9
kyoruni <kyoruni@gmail.com>
2020-01-23 16:11:35 +0900

    :wrench: #20 リサイズテスト

--- src/views/DragResize.vue
+++ src/views/DragResize.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="drag-resize">
-    <div style="background: red;height: 500px; width: 500px; border: 1px solid red; position: relative;">
-      <vue-draggable-resizable :w="500" :h="300" :min-width="500" :handles="['bm']" @resizing="onResize" :parent="true" :draggable="false" class="bg-info">
-        <p>X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}</p>
-      </vue-draggable-resizable>
+    <div>
+
+      <div class="div1">
+        <vue-draggable-resizable :w="500" :h="200" :min-height="200" :min-width="500" :handles="['bm']" @resizing="onResize" :parent="true" :draggable="false" style="background: rgba(0,0,0,0.5); padding: 20px;">
+          <p>X: {{ x1 }} / Y: {{ y1 }} - Width: {{ width1 }} / Height: {{ height1 }}</p>
+          div1
+        </vue-draggable-resizable>
+      </div>
+
+      <div class="div2">
+        <vue-draggable-resizable :w="500" :h="200" :min-width="500" :handles="['bm']" @resizing="onResize2" :parent="true" :draggable="true" style="background: rgba(255,0,0,0.5); padding: 20px;">
+          <p>X: {{ x2 }} / Y: {{ y2 }} - Width: {{ width2 }} / Height: {{ height2 }}</p>
+          div2
+        </vue-draggable-resizable>
+      </div>
     </div>
   </div>
 </template>
@@ -18,19 +29,55 @@ export default {
   },
   data () {
     return {
-      width: 0,
-      height: 0,
-      x: 0,
-      y: 0
+      width1: 0,
+      height1: 0,
+      x1: 0,
+      y1: 0,
+      width2: 0,
+      height2: 0,
+      x2: 0,
+      y2: 0
     }
   },
   methods: {
     onResize (x, y, width, height) {
       this.x = x
       this.y = y
-      this.width = width
-      this.height = height
+      this.width1 = width
+      this.height1 = height
+    },
+    onResize2 (x, y, width, height) {
+      this.x = x
+      this.y = y
+      this.width2 = width
+      this.height2 = height
     }
   }
 }
 </script>
+
+<style>
+.handle {
+  cursor: row-resize
+}
+</style>
+
+<style scoped>
+.div1 {
+  background: yellow;
+  height: 300px;
+  width: 500px;
+  border: 1px solid red;
+  position: relative;
+  z-index: 999
+}
+
+.div2 {
+  height: 300px;
+  width: 500px;
+  border: 1px solid blue;
+  position: relative;
+  top: 0px;
+  z-index: 888
+}
+</style>
