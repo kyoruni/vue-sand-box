c1e5c8e76d8d63a9c6e70553b30e35af0baa1666
kyoruni <kyoruni@gmail.com>
2020-01-23 11:45:59 +0900

    :wrench: #20 高さだけドラッグで変更できるように

--- src/views/DragResize.vue
+++ src/views/DragResize.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="drag-resize">
-    resize
+    <div style="background: red;height: 500px; width: 500px; border: 1px solid red; position: relative;">
+      <vue-draggable-resizable :w="500" :h="300" :min-width="500" :handles="['bm']" @resizing="onResize" :parent="true" :draggable="false" class="bg-info">
+        <p>X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}</p>
+      </vue-draggable-resizable>
+    </div>
   </div>
 </template>
+
+<script>
+import VueDraggableResizable from 'vue-draggable-resizable'
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
+
+export default {
+  components: {
+    VueDraggableResizable
+  },
+  data () {
+    return {
+      width: 0,
+      height: 0,
+      x: 0,
+      y: 0
+    }
+  },
+  methods: {
+    onResize (x, y, width, height) {
+      this.x = x
+      this.y = y
+      this.width = width
+      this.height = height
+    }
+  }
+}
+</script>
