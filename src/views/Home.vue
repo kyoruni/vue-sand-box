9443bdd01d3bd83b6d7be8109061ca460097d76e
kyoruni <kyoruni@gmail.com>
2020-02-04 13:33:02 +0900

    :construction: ホーム画面作成中

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
-    ほーむ
+    <div class="row">
+      <div class="col-10">
+        <div class="list-group col-4">
+          <a href="#" class="list-group-item list-group-item-action">test</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
