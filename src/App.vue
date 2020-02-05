65df5d7431ed985b5b45e2c840ee87dfe40a68a1
kyoruni <kyoruni@gmail.com>
2020-02-05 15:53:09 +0900

    :construction: 一旦ヘッダーメニューをコメントアウト

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <!-- ヘッダーメニュー -->
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand href="#">Vue.js sand-box</b-navbar-brand>
+    <!-- <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Vue.js sand-box</b-navbar-brand> -->
       <!-- メニューリンク -->
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
+      <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav> -->
         <!-- Home -->
-        <b-navbar-nav>
+        <!-- <b-navbar-nav>
           <b-nav-item to="/">目次</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
-    </b-navbar>
+    </b-navbar> -->
     <!-- メインページ -->
     <b-container fluid class="mt-2">
       <router-view/>
