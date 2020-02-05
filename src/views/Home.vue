c3b84940a6f343d1776d1844ce947635d5a75bd5
kyoruni <kyoruni@gmail.com>
2020-02-05 15:44:00 +0900

    :recycle: BootstrapVueの書き方に修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,15 @@
         <div class="col-4">
           <h6 class="font-weight-bold">お試し</h6>
           <div class="list-group">
-            <router-link to="/todo" class="list-group-item list-group-item-action">TODOリスト</router-link>
-            <router-link to="/parent" class="list-group-item list-group-item-action">親子コンポーネントの処理1</router-link>
-            <router-link to="/parent2" class="list-group-item list-group-item-action">親子コンポーネントの処理2</router-link>
-            <router-link to="/array-copy" class="list-group-item list-group-item-action">配列のコピー</router-link>
+            <b-link to="/todo" class="list-group-item list-group-item-action">TODOリスト</b-link>
+            <b-link to="/parent" class="list-group-item list-group-item-action">親子コンポーネントの処理1</b-link>
+            <b-link to="/parent2" class="list-group-item list-group-item-action">親子コンポーネントの処理2</b-link>
+            <b-link to="/array-copy" class="list-group-item list-group-item-action">配列のコピー</b-link>
           </div>
           <h6 class="font-weight-bold mt-2">ライブラリお試し</h6>
           <div class="list-group">
-            <router-link to="/draggable" class="list-group-item list-group-item-action">ドラッグ＆ドロップ</router-link>
-            <router-link to="/editor" class="list-group-item list-group-item-action">エディター表示</router-link>
+            <b-link to="/draggable" class="list-group-item list-group-item-action">ドラッグ＆ドロップ</b-link>
+            <b-link to="/editor" class="list-group-item list-group-item-action">エディター表示</b-link>
           </div>
         </div>
       </div>
